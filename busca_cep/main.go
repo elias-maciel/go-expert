bb6b7e0d65c8ec048f362760b28c327ea430fa91
Elias Maciel <eliasfsilvamaciel@gmail.com>
2026-02-13 19:39:44 -0300

    Add main.go with a basic HTTP server and handler

---
+++ busca_cep/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", BuscaCepHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Write([]byte("Hello, world!"))
+}
