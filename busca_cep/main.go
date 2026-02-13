87aa786548fb8a9dc1d39e500348364b52f6756c
Elias Maciel <eliasfsilvamaciel@gmail.com>
2026-02-13 19:51:41 -0300

    Improve error handling and response for BuscaCepHandler

--- busca_cep/main.go
+++ busca_cep/main.go
@@ -4,7 +4,10 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", BuscaCepHandler)
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 }
 
 func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,5 +15,15 @@ func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	w.Write([]byte("Hello, world!"))
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("Hello, world!"))
+	if err != nil {
+		return
+	}
 }
