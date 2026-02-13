6c8bf1c24fc8d28e2ba6fa1eccf47ffe4019067e
Elias Maciel <eliasfsilvamaciel@gmail.com>
2026-02-13 20:05:38 -0300

    Add BuscaCep function to fetch address data from ViaCEP API

--- busca_cep/main.go
+++ busca_cep/main.go
@@ -1,6 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Unidade     string `json:"unidade"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+}
 
 func main() {
 	http.HandleFunc("/", BuscaCepHandler)
@@ -20,10 +36,33 @@ func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	cep, err := BuscaCep(cepParam)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte("Hello, world!"))
+	err = json.NewEncoder(w).Encode(cep)
 	if err != nil {
 		return
 	}
+}
+
+func BuscaCep(cep string) (*ViaCEP, error) {
+	resp, error := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+	defer resp.Body.Close()
+	body, error := ioutil.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+	var c ViaCEP
+	error = json.Unmarshal(body, &c)
+	if error != nil {
+		return nil, error
+	}
+	return &c, nil
 }
