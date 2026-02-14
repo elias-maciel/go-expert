0c4382e67dfdef37a06e4fa6882506969496a59c
Elias Maciel <eliasfsilvamaciel@gmail.com>
2026-02-13 21:12:59 -0300

    Add HTML template and main function to display course information

---
+++ 7-templates/3/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	t := template.Must(template.New("index.html").ParseFiles("./templates/index.html"))
+	err := t.Execute(os.Stdout, Cursos{
+		{"Go", 40},
+		{"Python", 30},
+		{"JavaScript", 20},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
